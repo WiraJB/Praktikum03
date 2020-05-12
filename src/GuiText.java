76665f066ff51ba314e97b8be85c52c553be5f32
WiraJB <wirajulian88@gmail.com>
2020-05-12 20:55:06 +0800

    Initial commit

---
+++ src/GuiText.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GuiText extends JFrame {
+    GuiText(){
+        setTitle("Form1");
+        setSize(690,480);
+    }
+
+    public static void main(String[] args) {
+        GuiText guiByText = new GuiText();
+        guiByText.setVisible(true);
+    }
+
+}
